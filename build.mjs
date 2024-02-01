40faf6563a9e32e73c196c405eccd72b0db996d3
Andrew Bulat <andrii.bulat@gmail.com>
2024-02-01 23:51:32 +0000

    Removes top level await from build

--- build.mjs
+++ build.mjs
@@ -1,5 +1,6 @@
 import esbuild from "esbuild";
 import { execSync } from "child_process";
+
 const shouldWatch = process.argv.includes("watch");
 const finalizer = {
   name: "finalizer",
@@ -10,17 +11,25 @@ const finalizer = {
   },
 };
 
-const ctx = await esbuild[shouldWatch ? "context" : "build"]({
-  entryPoints: ["./src/index.ts"],
-  outfile: "./dist/index.js",
-  platform: "node",
-  target: "es6",
-  format: "cjs",
-  bundle: true,
-  minify: !shouldWatch,
-  plugins: [finalizer],
-});
+async function build() {
+  const ctx = await esbuild[shouldWatch ? "context" : "build"]({
+    entryPoints: ["./src/index.ts"],
+    outfile: "./dist/index.js",
+    platform: "node",
+    target: "es6",
+    format: "cjs",
+    bundle: true,
+    minify: !shouldWatch,
+    plugins: [finalizer],
+  });
 
-if (shouldWatch) {
-  await ctx.watch();
+  if (shouldWatch) {
+    await ctx.watch();
+  }
 }
+
+build()
+  .then()
+  .catch((err) => {
+    throw err;
+  });
